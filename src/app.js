75eaa1925d904b62006ef2a134f646a245a468e2
Jose <jm.bebor@gmail.com>
2025-04-23 09:31:30 +0000

    Intento de rama

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let who = ['You mama', 'My dinosaur', 'The president', 'My brain'];
 let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
+let what = ['my a**', 'my d***', 'my pc'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
 var excusa= document.getElementById("excuse");
