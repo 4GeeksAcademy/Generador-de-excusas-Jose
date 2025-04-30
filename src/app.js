c5951bbf1b35c7e7b88db9df8faf3f6001ae0711
Jose <jm.bebor@gmail.com>
2025-04-30 10:47:45 +0000

    Functional button added

--- src/app.js
+++ src/app.js
@@ -5,18 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  let who = ['You mama', 'My dinosaur', 'The president', 'My brain'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my a**', 'my d***', 'my pc'];
-let when = ['before the class', 'when I was sleeping', 'while I was in the bathroom', 'during my lunch', 'while I was praying'];
+window.onload = function () {
+  const who = ['You mama', 'My dinosaur', 'The president', 'My brain'];
+  const action = ['ate', 'peed', 'crushed', 'broke'];
+  const what = ['my a**', 'my d***', 'my pc'];
+  const when = ['before the class', 'when I was sleeping', 'while I was in the bathroom', 'during my lunch', 'while I was praying'];
 
-var excusa= document.getElementById("excuse");
+  let excusa = document.getElementById("excuse");
 
-excusa.innerHTML=who[Math.floor(Math.random()*who.length)]+" "+action[Math.floor(Math.random()*action.length)]+" "+what[Math.floor(Math.random()*what.length)]+" "+when[Math.floor(Math.random()*when.length)];
+  excusa.innerHTML = who[Math.floor(Math.random() * who.length)] + " " + action[Math.floor(Math.random() * action.length)] + " " + what[Math.floor(Math.random() * what.length)] + " " + when[Math.floor(Math.random() * when.length)];
 
 
 
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+function generarExcusa() {
+  const who = ['You mama', 'My dinosaur', 'The president', 'My brain'];
+  const action = ['ate', 'peed', 'crushed', 'broke'];
+  const what = ['my a**', 'my d***', 'my pc'];
+  const when = ['before the class', 'when I was sleeping', 'while I was in the bathroom', 'during my lunch', 'while I was praying'];
+  
+  let excusa = document.getElementById("excuse");
+  return excusa.innerHTML = who[Math.floor(Math.random() * who.length)] + " " + action[Math.floor(Math.random() * action.length)] + " " + what[Math.floor(Math.random() * what.length)] + " " + when[Math.floor(Math.random() * when.length)];
+}
+
+document.getElementById("boton").onclick=generarExcusa;
